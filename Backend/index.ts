ea2127a7436600aad00dde2eea95965c1b40e5f4
07Abhinav <108852841+07Abhinav@users.noreply.github.com>
2025-01-22 23:29:52 +0530

    Update index.ts

--- Backend/index.ts
+++ Backend/index.ts
@@ -241,7 +241,7 @@ const errorHandler = (
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
